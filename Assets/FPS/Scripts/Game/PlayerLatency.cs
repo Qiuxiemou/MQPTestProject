02be517196a047ff12806e30eac149fb298456a5
Qiuxiemou <qs2231339437@gmail.com>
2026-03-06 15:54:52 -0500

    BUG FIX
    player aimpoint position bug fix
    bot shoot normal distribution bug fix

--- Assets/FPS/Scripts/Game/PlayerLatency.cs
+++ Assets/FPS/Scripts/Game/PlayerLatency.cs
@@ -1,25 +1,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Unity.FPS.Game
+public class PlayerLatency : MonoBehaviour
 {
-    public class PlayerLatency : MonoBehaviour
+    public Transform player;
+    public float latency = 0.15f;
+    public Vector3 offset = new Vector3(0f, 1.6f, 0f);
+
+    private readonly Queue<(float time, Vector3 pos)> buffer = new();
+    private bool _initialized = false;
+    private Vector3 initialPosition;
+    float startTime;
+
+    void LateUpdate()
     {
-        public Transform player;
-        public float latency = 0.15f;
+        if (!player) return;
 
-        private Queue<(float time, Vector3 pos)> buffer = new();
-
-        void LateUpdate()
+        // Initialize: lock aimpoint to first sampled position
+        if (!_initialized)
         {
-            if (!player) return;
+            initialPosition = player.position + offset;
+            startTime = Time.time;
+            _initialized = true;
+        }
 
-            buffer.Enqueue((Time.time, player.position));
+        buffer.Enqueue((Time.time, player.position+ offset));
 
-            while (buffer.Count > 0 && Time.time - buffer.Peek().time >= latency)
-            {
-                transform.position = buffer.Dequeue().pos;
-            }
+        if (Time.time - startTime < latency)
+        {
+            transform.localPosition = transform.parent.InverseTransformPoint(initialPosition); ;
+            return;
+        }
+            
+        // Output delayed samples
+        while (buffer.Count > 0 && Time.time - buffer.Peek().time >= latency)
+        {
+            transform.localPosition = transform.parent.InverseTransformPoint(buffer.Dequeue().pos); ;
         }
     }
 }
